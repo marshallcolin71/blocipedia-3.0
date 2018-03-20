94320f633ccfc0f289fb4e6d41e1a9a14f812808
Colin <marshallcolin71@gmail.com>
2018-03-20 12:15:27 -0400

    added user roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315174503) do
+ActiveRecord::Schema.define(version: 20180320152258) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,10 +26,12 @@ ActiveRecord::Schema.define(version: 20180315174503) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["user_id"], name: "index_users_on_user_id"
 
   create_table "wikis", force: :cascade do |t|
     t.string   "title"

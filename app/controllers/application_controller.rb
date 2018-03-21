c8a108ef1e960ba3c50a1dc4941ef691f90ee1cb
Colin <marshallcolin71@gmail.com>
2018-03-21 03:05:51 -0400

    a couple of syntax changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  include pundit
+  include Pundit
   protect_from_forgery with: :exception
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20180320152258) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "role"
+    t.integer  "user_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
